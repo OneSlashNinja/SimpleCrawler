641f9f48133cb57054fb9929b7f0e55cdf1b2f12
QiyueSheng <dereksheng1226@gmail.com>
2017-02-12 18:47:08 -0500

    add the functionalities to download the pictures from www.pexels.com and ignore pics for git

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ var request = require('request'),
     cheerio = require('cheerio'),
     fs = require('fs');
 
-request('https://unsplash.com/search/food', function(error, response, body){
+request('https://www.pexels.com/search/HD%20wallpaper/', function(error, response, body){
     if(!error && response.statusCode == 200){
         var $ = cheerio.load(body);
-        console.log(body);
-        console.log($);
+
+        var targets = $('img','.photo-item');
+
+        var imageUrls = [];
+
+        for(var i = 0; i < targets.length; i++){
+            imageUrls.push(targets[i].attribs.src);
+            //console.log(targets[0].attribs.src);
+        }
+
+        //console.log(imageUrls);
+        for(var i = 0; i < imageUrls.length; i++){
+            request(imageUrls[i]).pipe(fs.createWriteStream('./downloads/Wallpaper'+ i + '.jpg'));
+        }
     }
 });
+
+// var downloadPics = function(imageUrlList){
+    
+// }
+
+//request('https://unsplash.com/collections/191435/glorious-food').pipe(fs.createWriteStream('./downloads/test.html'));
