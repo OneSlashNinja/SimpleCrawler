bf4d21bb5c545bbf84e8676a6c47ddcd837cfc0b
QiyueSheng <dereksheng1226@gmail.com>
2017-02-13 00:24:14 -0500

    switch to crawler another site... you know

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
 var request = require('request'),
+    path = require('path'),
     cheerio = require('cheerio'),
     fs = require('fs');
 
-request('https://www.pexels.com/search/HD%20wallpaper/', function(error, response, body){
+var targetUrl = "http://mt.91.com/meinv/relaxiezhen/";
+var finalUrls = [];
+
+request(targetUrl, function(error, response, body){
     if(!error && response.statusCode == 200){
         var $ = cheerio.load(body);
 
-        var targets = $('img','.photo-item');
+        var targets = $('img','.yb_d > ul > li > a').parent();
 
-        var imageUrls = [];
+        console.log(targets[0].attribs.href);
+
+        //topLevel list
+        var collectionUrlList = [];
 
         for(var i = 0; i < targets.length; i++){
-            imageUrls.push(targets[i].attribs.src);
-            //console.log(targets[0].attribs.src);
+            //collectionUrlList.push(targets[i].attribs.href);
+
+            request(targets[i].attribs.href, getFinalImgUrls);
         }
 
-        //console.log(imageUrls);
-        for(var i = 0; i < imageUrls.length; i++){
-            request(imageUrls[i]).pipe(fs.createWriteStream('./downloads/Wallpaper'+ i + '.jpg'));
-        }
+        // downloadPics(imageUrls);
     }
 });
 
-// var downloadPics = function(imageUrlList){
-    
-// }
+var getFinalImgUrls = function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var $ = cheerio.load(body);
 
-//request('https://unsplash.com/collections/191435/glorious-food').pipe(fs.createWriteStream('./downloads/test.html'));
+        var targets = $('img', '.kq > a');
+
+        finalUrls.push(targets[0].attribs.src);
+
+        //console.log(targets[0].attribs.src);
+        downloadPic(targets[0].attribs.src);
+    }
+}
+
+var downloadPic = function(imageUrl){
+    var filename = path.basename(imageUrl);
+    request(imageUrl).pipe(fs.createWriteStream('./downloads/' + filename));
+}
+
+var downloadPics = function(imageUrls){
+    for(var i = 0; i < imageUrls.length; i++){
+        downloadPic(imageUrls[i]);
+    }
+}
